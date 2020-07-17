19ec9ceef384a63a6853c20b37826c2024ccfece
Muhamad Fikri Mubarok <muhamadfikri64@yahoo.co.id>
2020-07-17 10:43:27 +0700

    membuat kerangka html di index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP E-Commerce</title>
+</head>
+<body>
+    <header>
+        <div class="jumbotron">
+        </div>
+        <nav>
+        
+        </nav>
+    </header>
+    <main>
+        <?php
+            $filename = "$page.php";
+            if (file_exists($filename)) {
+                include_once($filename)
+            }else{
+                include_once("main.php")
+            }
+        ?>
+    </main>
+    <footer>
+    </footer>
+    
+</body>
+</html>
